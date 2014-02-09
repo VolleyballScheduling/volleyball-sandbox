0c4088e2444140a7b5598fa4e9426d61fe9b4a13
David Durost <david.durost@gmail.com>
2014-02-09 18:12:25 -0500

    initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+Summer Camp Scheduling System
+
+Online course management system primarily aimed at easing the logistics of scheduling multiple attendees, groups across a multitude of facilities.
---
+++ app/AppKernel.php
@@ -0,0 +1,60 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
+
+            new FOS\UserBundle\FOSUserBundle(),
+            new PUGX\MultiUserBundle\PUGXMultiUserBundle(),
+            
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            
+            new Liip\ImagineBundle\LiipImagineBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            
+            new Volleyball\UserBundle\VolleyballUserBundle(),
+            new Volleyball\PasselBundle\VolleyballPasselBundle(),
+            new Volleyball\OrganizationBundle\VolleyballOrganizationBundle(),
+            new Volleyball\FacilityBundle\VolleyballFacilityBundle(),
+            new Volleyball\EnrollmentBundle\VolleyballEnrollmentBundle(),
+            new Volleyball\CourseBundle\VolleyballCourseBundle(),
+            new Volleyball\ReportBundle\VolleyballReportBundle(),
+            new Volleyball\UtilityBundle\VolleyballUtilityBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new JMS\DebuggingBundle\JMSDebuggingBundle($this);
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
